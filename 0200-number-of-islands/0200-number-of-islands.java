e189c8e590200feb1f6bfaca49e1bb55ba6679cd
Kuldeep Yadav <96827805+yadavkuldeep1017@users.noreply.github.com>
2025-08-17 20:38:57 +0530

    Time: 4 ms (49.23%), Space: 52.2 MB (21.85%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,37 @@
+class Solution {
+    boolean[][] visited;
+    int m;
+    int n;
+
+    public int dfs(char[][] grid,int row, int col){
+        if(row == -1 || row == m || col ==-1 || col == n || visited[row][col]){
+            return 0;
+        }
+        visited[row][col] = true;
+        if(grid[row][col] == '0'){
+            return 0;
+        }
+        int[] rowArray = {-1, 1, 0 ,0};
+        int[] colArray = {0 ,0, -1 ,1};
+        for(int i = 0; i < 4; i++){
+            dfs(grid, row + rowArray[i], col + colArray[i]);
+        }
+        return 1;
+        
+    }
+
+    public int numIslands(char[][] grid) {
+        int ans = 0;
+        m = grid.length;
+        n = grid[0].length;
+        visited = new boolean[m][n];
+        for(int i = 0; i < m; i++){
+            for( int j = 0; j < n; j++){
+                if(!visited[i][j]){
+                    ans += dfs(grid, i, j);
+                }
+            }
+        }
+        return ans;
+    }
+}
