65b50264f036e5c5782e1e84073b1f16936cf9f1
Kuldeep Yadav <96827805+yadavkuldeep1017@users.noreply.github.com>
2025-09-12 22:29:54 +0530

    Time: 15 ms (56.08%), Space: 55.2 MB (94.52%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,34 +1,33 @@
 class Solution {
     boolean[] visited;
     boolean[] safe;
-    public boolean isSafePath(int[][] graph, int v, List<Integer> path){
+
+    public void isSafePath(int[][] graph, int v, List<Integer> path) {
         visited[v] = true;
         boolean isSafe = true;
-        for(int i = 0; i< graph[v].length; i++){
-            if(visited[graph[v][i]]){
-                isSafe = isSafe && safe[graph[v][i]];
-            }
-            else if(!isSafePath(graph,graph[v][i],path)){
-                isSafe = false;
-            }
+        for (int i = 0; i < graph[v].length; i++) {
+            if (!visited[graph[v][i]]) {
+                isSafePath(graph, graph[v][i], path);
+            } 
+            isSafe = isSafe && safe[graph[v][i]];
         }
-        if(isSafe){
-            if(!safe[v]){
+        if (isSafe) {
+            if (!safe[v]) {
                 path.add(v);
             }
-            return safe[v] = true;
+            safe[v] = true;
         }
-        return false;
     }
+
     public List<Integer> eventualSafeNodes(int[][] graph) {
         List<Integer> ans = new ArrayList();
         int v = graph.length;
         visited = new boolean[v];
         safe = new boolean[v];
-        for(int i = 0; i < v; i++){
-            if(!visited[i]){
+        for (int i = 0; i < v; i++) {
+            if (!visited[i]) {
                 List<Integer> path = new ArrayList();
-                isSafePath(graph,i,path);
+                isSafePath(graph, i, path);
                 ans.addAll(path);
             }
         }
