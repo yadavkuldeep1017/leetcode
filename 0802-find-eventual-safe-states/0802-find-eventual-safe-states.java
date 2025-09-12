24240f38972454e034df395792477bbc8d6227f0
Kuldeep Yadav <96827805+yadavkuldeep1017@users.noreply.github.com>
2025-09-12 22:33:04 +0530

    Time: 14 ms (58%), Space: 56 MB (12.41%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -2,7 +2,7 @@ class Solution {
     boolean[] visited;
     boolean[] safe;
 
-    public void isSafePath(int[][] graph, int v, List<Integer> path) {
+    public void isSafePath(int[][] graph, int v, Set<Integer> path) {
         visited[v] = true;
         boolean isSafe = true;
         for (int i = 0; i < graph[v].length; i++) {
@@ -12,26 +12,21 @@ class Solution {
             isSafe = isSafe && safe[graph[v][i]];
         }
         if (isSafe) {
-            if (!safe[v]) {
-                path.add(v);
-            }
+            path.add(v);
             safe[v] = true;
         }
     }
 
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        List<Integer> ans = new ArrayList();
+        Set<Integer> path = new TreeSet();
         int v = graph.length;
         visited = new boolean[v];
         safe = new boolean[v];
         for (int i = 0; i < v; i++) {
             if (!visited[i]) {
-                List<Integer> path = new ArrayList();
                 isSafePath(graph, i, path);
-                ans.addAll(path);
             }
         }
-        Collections.sort(ans);
-        return ans;
+        return new ArrayList(path);
     }
 }
