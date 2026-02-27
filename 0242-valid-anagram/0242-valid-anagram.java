a9aff7ad0fe9b5dbc64989a8511e7fa280fd6d99
Kuldeep Yadav <96827805+yadavkuldeep1017@users.noreply.github.com>
2026-02-27 20:41:57 +0530

    Time: 4 ms (80.53%), Space: 44.6 MB (67%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        int[] ch = new int[26];
+        Arrays.fill(ch, 0);
+        for(int i = 0; i < s.length(); i++){
+            ch[s.charAt(i) - 'a']++;
+        }
+        for(int i = 0; i < t.length(); i++){
+            ch[t.charAt(i) - 'a']--;
+        }
+        for(int i = 0; i < 26; i++){
+            if(ch[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
