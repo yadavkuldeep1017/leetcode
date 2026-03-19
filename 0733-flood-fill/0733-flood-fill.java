5502fa526acf73835596d4fed894b2f7083bff46
Kuldeep Yadav <96827805+yadavkuldeep1017@users.noreply.github.com>
2026-03-19 22:16:37 +0530

    Time: 1 ms (44.45%), Space: 47 MB (21.79%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,27 +1,30 @@
 class Solution {
+    public void dfs(int sr, int sc, int m, int n, int[][]image, boolean[][] visited){
+        visited[sr][sc] = true;
+        int[] rowArr = {-1, 1, 0, 0};
+        int[] colArr = {0, 0, -1, 1};
+        for(int i = 0; i < 4; i++){
+            int nRow = sr + rowArr[i];
+            int nCol = sc + colArr[i];
+            if(nRow >= 0 && nRow < m && nCol >= 0 && nCol < n && !visited[nRow][nCol] && image[nRow][nCol] == image[sr][sc]){
+                dfs(nRow, nCol, m, n, image, visited);
+            }
+        }
+    }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int m = image.length;
-        int n = image[0].length;
-        boolean[][] visited = new boolean[m][n];
-        int imageElement = image[sr][sc];
-        Queue<Pair<Integer,Integer>> q = new ArrayDeque();
-        q.add(new Pair(sr,sc));
-        int[] drow = {-1,1,0,0};
-        int[] dcol = {0,0,-1,1};
-        while(!q.isEmpty()){
-            Pair<Integer,Integer> pair = q.remove();
-            int row = pair.getKey();
-            int col = pair.getValue();
-            image[row][col] = color;
-            visited[row][col] = true;
-            for(int i = 0; i < 4; i++){
-                int currentRow = row+drow[i];
-                int currentCol = col+dcol[i];
-                if(currentRow>=0 && currentRow<m && currentCol>=0 && currentCol<n &&
-                    image[currentRow][currentCol] == imageElement &&
-                    !visited[currentRow][currentCol]){
-                    q.add(new Pair(currentRow , currentCol));
+        if(image[sr][sc] != color){
+            int m = image.length;
+            int n = image[0].length;
+            boolean[][] visited = new boolean[m][n];
+            dfs(sr, sc, m, n, image, visited);
+            for(int i = 0; i< m; i++){
+                for(int j = 0; j < n; j++){
+                    // System.out.print(visited[i][j]+" ");
+                    if(visited[i][j]){
+                        image[i][j] = color;
+                    }
                 }
+                // System.out.println();
             }
         }
         return image;
