10cc378b478ad4c0acb508df5bc5efe2c1b22a29
Kuldeep Yadav <96827805+yadavkuldeep1017@users.noreply.github.com>
2026-03-30 07:16:20 +0530

    Time: 10 ms (23.42%), Space: 46.6 MB (60.74%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,70 +1,43 @@
 class Solution {
-    static class Pair{
-        int x;
-        int distance;
-        Pair(int x, int distance){
-            this.x = x;
-            this.distance = distance;
-        }
-    }
-    static class Flight{
-        int index;
-        int distance;
-        int stop;
-        Flight(int index, int distance, int stop){
-            this.index = index;
-            this.distance = distance;
-            this.stop = stop;
-        }
-    }
-    static class FlightComparator implements Comparator<Flight>{
-        public int compare(Flight f1, Flight f2){
-            if(f1.stop == f2.stop){
-                return Integer.compare(f1.distance, f2.distance);
-            }
-            return Integer.compare(f1.stop, f2.stop);
-        }
-    }
-    public List<List<Pair>> getAdjList(int[][] flights, int n){
-        List<List<Pair>> adjList = new ArrayList();
+
+    public List<List<Pair<Integer,Integer>>> getAdjList(int n, int[][] flights){
+        List<List<Pair<Integer,Integer>>> adjList = new ArrayList();
         for(int i = 0; i < n; i++){
             adjList.add(new ArrayList());
         }
         for(int i = 0; i < flights.length; i++){
-            adjList.get(flights[i][0]).add(new Pair(flights[i][1],flights[i][2]));
+            adjList.get(flights[i][0]).add(new Pair(flights[i][1], flights[i][2]));
         }
         return adjList;
     }
+
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        List<List<Pair>> adjList = getAdjList(flights, n);
-        // for(List<Pair> listpair:adjList){
-        //     for(Pair pair:listpair){
-        //       System.out.print(pair.x+","+pair.distance+":");              
-        //     }
-        //     System.out.println();
-        // }
+        List<List<Pair<Integer,Integer>>> adjList = getAdjList(n, flights);
+        // System.out.println(adjList);
         int[] distance = new int[n];
         Arrays.fill(distance, Integer.MAX_VALUE);
-        PriorityQueue<Flight> pq = new PriorityQueue(new FlightComparator());
-        pq.add(new Flight(src, 0, 0));
+        Queue<int[]> q = new PriorityQueue<>((a, b) -> {
+            if(a[0] == b[0]){
+                return Integer.compare(a[2], b[2]);
+            }
+            return Integer.compare(a[0], b[0]);
+        });
+        q.add(new int[]{0, src, 0});
         distance[src] = 0;
-        while(!pq.isEmpty()){
-            Flight flight = pq.poll();
-            for(Pair pair:adjList.get(flight.index)){
-                int currDistance = pair.distance + flight.distance;
-                if(currDistance < distance[pair.x]){
-                    distance[pair.x] = currDistance;
-                    if(k > flight.stop){
-                        pq.add(new Flight(pair.x,currDistance,flight.stop+1));
+        while(!q.isEmpty()){
+            int[] flight = q.poll();
+            if(flight[0] <= k){
+                for(Pair<Integer, Integer> pair: adjList.get(flight[1])){
+                    int node = pair.getKey();
+                    int dist = pair.getValue();
+                    int newDistance = dist + flight[2];
+                    if(distance[node] > newDistance){    
+                        q.add(new int[]{flight[0] + 1, node, newDistance});
+                        distance[node] = newDistance;
                     }
                 }
             }
-
         }
-        // for(int num:distance){
-        //     System.out.println(num);
-        // }
-        return distance[dst] == Integer.MAX_VALUE ? -1:
-        distance[dst];
+        return distance[dst] == Integer.MAX_VALUE ? -1 : distance[dst];
     }
 }
