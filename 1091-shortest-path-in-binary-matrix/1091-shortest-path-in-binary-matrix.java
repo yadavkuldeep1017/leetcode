0393b8c5e08fa6ed8d96fcd6bc17275fb40a4d28
Kuldeep Yadav <96827805+yadavkuldeep1017@users.noreply.github.com>
2026-04-06 22:28:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,38 +1,48 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
         int n = grid.length;
-        if(grid[0][0] == 1 || grid[n - 1][n - 1] == 1){
+
+        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1) {
             return -1;
         }
-        int[][] distance = new int[n][n];
-        for(int i = 0; i < n; i++){
-            Arrays.fill(distance[i], Integer.MAX_VALUE);
-        }
-        Queue<int[]> q = new LinkedList();//{i, j, dist}
-        q.add(new int[]{0, 0, 1});
-        distance[0][0] = 1;
-        while(!q.isEmpty()){
-            int[] path = q.poll();
-            int r = path[0];
-            int c = path[1];
-            int d = path[2];
-            int[] rowArr = {1,-1, 0, 0, 1, 1, -1, -1};
-            int[] colArr = {0, 0, 1, -1, 1, -1, 1, -1};
-            for(int i = 0; i < 8; i++){
-                int nr = r + rowArr[i];
-                int nc = c + colArr[i];
-                if(nr >= 0 && nr < n && nc >= 0 && nc < n && grid[nr][nc] == 0 && distance[nr][nc] > d + 1){
-                    distance[nr][nc] = d + 1;
-                    q.add(new int[]{nr, nc, d + 1});
+
+        int[][] directions = {
+            {1,0},{-1,0},{0,1},{0,-1},
+            {1,1},{1,-1},{-1,1},{-1,-1}
+        };
+
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0, 0});
+        grid[0][0] = 1; // mark visited
+
+        int distance = 1;
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+
+            for (int i = 0; i < size; i++) {
+                int[] cell = q.poll();
+                int r = cell[0];
+                int c = cell[1];
+
+                if (r == n - 1 && c == n - 1) {
+                    return distance;
+                }
+
+                for (int[] dir : directions) {
+                    int nr = r + dir[0];
+                    int nc = c + dir[1];
+
+                    if (nr >= 0 && nr < n && nc >= 0 && nc < n && grid[nr][nc] == 0) {
+                        q.add(new int[]{nr, nc});
+                        grid[nr][nc] = 1; // mark visited
+                    }
                 }
             }
+
+            distance++;
         }
-        // for(int i = 0; i < n; i++){
-        //     for(int j = 0; j < n; j++){
-        //         System.out.print(distance[i][j] + " ");
-        //     }
-        //     System.out.println();
-        // }
-        return distance[n - 1][n - 1] == Integer.MAX_VALUE ? -1 : distance[n - 1][n - 1];
+
+        return -1;
     }
 }
