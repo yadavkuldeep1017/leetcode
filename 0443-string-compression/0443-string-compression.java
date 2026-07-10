d67bd5e5f68762f124e231de110d891a7c960ec9
Kuldeep Yadav <96827805+yadavkuldeep1017@users.noreply.github.com>
2026-07-10 21:54:02 +0530

    Time: 3 ms (11.43%), Space: 45.3 MB (65.38%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int compress(char[] chars) {
+        int ans = 0;
+        int n = chars.length;
+        int i = 0;
+        int j = 0;
+        while(i < n){
+            int count = 1;
+            while(i + 1 < n && chars[i] == chars[i + 1]){
+                count ++;
+                i++;
+            }
+            chars[j++] = chars[i];
+            if(count == 1){
+                ans++;
+            }
+            else{
+                String strCount = String.valueOf(count);
+                ans += strCount.length() + 1;
+                for(char chCount : strCount.toCharArray()){
+                    chars[j++] = chCount;
+                }
+            }
+            i ++;
+        }
+        System.out.println(ans);
+        return ans;
+    }
+}
