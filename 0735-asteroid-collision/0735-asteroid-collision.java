8b359c0c872245484146e8f114cbc5ad6d80b81d
Kuldeep Yadav <96827805+yadavkuldeep1017@users.noreply.github.com>
2026-07-16 21:29:41 +0530

    Time: 2 ms (98.23%), Space: 46.7 MB (84.01%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,47 +1,35 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 class Solution {
-
-    public boolean isSame(int a, int b){
-        return  (a < 0) || (a >= 0 && b>= 0) || (a < 0 && b < 0);
-    }
-
     public int[] asteroidCollision(int[] asteroids) {
-        Stack<Integer> stack = new Stack();
-        int n = asteroids.length;
-        for(int i = 0; i < n; i++){
-            if(!stack.isEmpty()){
-                boolean isBreak = false;
-                while(!stack.isEmpty()){
-                    int peek = stack.peek();
-                    if(isSame(peek, asteroids[i])){
-                        stack.push(asteroids[i]);
-                        isBreak = true;
-                        break;
-                    }     
-                    if(peek + asteroids[i] == 0){
-                        stack.pop();
-                        isBreak = true;
-                        break;
-                    }
-                    if(Math.abs(peek) > Math.abs(asteroids[i])){
-                        isBreak = true;
-                        break;
-                    }  
-                    stack.pop();
-                }
-                if(!isBreak){
-                    stack.push(asteroids[i]);
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int a : asteroids) {
+            boolean alive = true;
+
+            while (alive && !stack.isEmpty() && stack.peekLast() > 0 && a < 0) {
+                int top = stack.peekLast();
+
+                if (top < -a) {
+                    stack.pollLast();      // top explodes, keep checking
+                } else if (top == -a) {
+                    stack.pollLast();      // both explode
+                    alive = false;
+                } else {
+                    alive = false;         // current asteroid explodes
                 }
             }
-            else{
-                stack.push(asteroids[i]);
+
+            if (alive) {
+                stack.addLast(a);
             }
-            // System.out.println(stack);
         }
-        int sz = stack.size();
-        int[] ans = new int[sz];
-        int i = sz - 1;
-        while(!stack.isEmpty()){
-            ans[i--] = stack.pop();
+
+        int[] ans = new int[stack.size()];
+        int i = 0;
+        for (int x : stack) {
+            ans[i++] = x;
         }
         return ans;
     }
