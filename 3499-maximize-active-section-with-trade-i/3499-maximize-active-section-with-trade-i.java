d78ca0a968bfdca93f73d7411e4e4eb9bb0cbc72
Kuldeep Yadav <96827805+yadavkuldeep1017@users.noreply.github.com>
2026-07-26 11:48:02 +0530

    Time: 55 ms (85.85%), Space: 48.2 MB (31.94%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.java
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.java
@@ -2,7 +2,8 @@ class Solution {
     public int maxActiveSectionsAfterTrade(String s) {
         int n = s.length();
         int total1 = 0;
-        for(char ch: s.toCharArray()){
+        char[] arr = s.toCharArray();
+        for(char ch: arr){
             total1 += ch - '0';
         }
         int ans = total1;
@@ -10,24 +11,23 @@ class Solution {
         int i = s.indexOf('0');
         while(i >= 0 && i < n){
             int startIndex = i;
-            while(i < n && s.charAt(i) != '1'){
+            while(i < n && arr[i] != '1'){
                 i++;
             }
             int countOne = 0;
-            while(i < n && s.charAt(i) != '0'){
+            while(i < n && arr[i] != '0'){
                 countOne++;
                 i++;
             }
             int tempPrev0 = 0;
-            while(i < n && s.charAt(i) != '1'){
+            while(i < n && arr[i] != '1'){
                 tempPrev0++;
                 i++;
             }
             if(countOne > 0 && tempPrev0 > 0){
                 ans = Math.max(ans, i - startIndex - countOne + total1 + prev0);
+                prev0 = tempPrev0;
             }
-            prev0 = tempPrev0;
-            // System.out.println(i+" "+startIndex+" "+ans+" "+countOne+" "+prev0+" "+total1);
         }
         return ans;
     }
