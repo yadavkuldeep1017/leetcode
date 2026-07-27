ce31cf39cb9cb70656561f4bc89b2e869bab06b6
Kuldeep Yadav <96827805+yadavkuldeep1017@users.noreply.github.com>
2026-07-27 21:13:26 +0530

    Time: 7 ms (61.94%), Space: 48.5 MB (24.16%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
@@ -2,11 +2,9 @@ class Solution {
     public int nearestExit(char[][] maze, int[] entrance) {
         int m = maze.length;
         int n = maze[0].length;
-        boolean[][] visited = new boolean[m][n];
         Queue<int[]> q = new ArrayDeque();
-        q.add(new int[]{entrance[0], entrance[1]});
-        visited[entrance[0]][entrance[1]] = true;
-        int path = 1;
+        q.add(new int[]{entrance[0], entrance[1], 0});
+        maze[entrance[0]][entrance[1]] = '+';
         int[] row = {-1, 1, 0, 0};
         int[] col = {0, 0, 1, -1};
         while(!q.isEmpty()){
@@ -16,16 +14,16 @@ class Solution {
                 for(int i = 0; i < 4; i++){
                     int newRow = mz[0] + row[i];
                     int newCol = mz[1] + col[i];
-                    if(newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && maze[newRow][newCol] == '.' && !visited[newRow][newCol]){
+                    int newDistance = mz[2] + 1;
+                    if(newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && maze[newRow][newCol] == '.'){
                         if(newRow == 0 || newRow == m - 1 || newCol == 0 || newCol == n - 1){
-                            return path;
+                            return newDistance;
                         }
-                        q.add(new int[]{newRow, newCol});
-                        visited[newRow][newCol] = true;
+                        q.add(new int[]{newRow, newCol, newDistance});
+                        maze[newRow][newCol] = '+';
                     }
                 }
             }
-            path++;
         }
         return -1;
     }
