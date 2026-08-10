1d4826f5c6d48ad29a67aa94c7bc90e28f78c36a
Kuldeep Yadav <96827805+yadavkuldeep1017@users.noreply.github.com>
2026-08-10 20:58:56 +0530

    Time: 37 ms (53.26%), Space: 50.8 MB (38.12%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.java
+++ 1510-stone-game-iv/1510-stone-game-iv.java
@@ -1,27 +1,23 @@
 class Solution {
 
-    public int isAliceWin(int n, boolean[] square, int[] dp){
+    public int isAliceWin(int n, int[] dp){
         if(n == 1){
             return 1;
         } 
         if(dp[n] != -1){
             return dp[n];
         }
-        for(int i = n; i >= 1; i--){
-            if(square[i] && (n - i == 0 || 
-            (isAliceWin(n - i, square, dp) == 0))){
+        for(int i = 1; i * i <= n; i++){
+            int num = i * i;
+            if(n - num == 0 || isAliceWin(n - num, dp) == 0){
                 return dp[n] = 1;
             }
         }
         return dp[n] = 0;
     }
     public boolean winnerSquareGame(int n) {
-        boolean[] square = new boolean[n + 1];
-        for (int i = 1; i * i <= n; i++) {
-            square[i * i] = true;
-        }
         int[] dp = new int[n + 1];
         Arrays.fill(dp , -1);
-        return isAliceWin(n, square, dp) == 0 ? false : true;
+        return isAliceWin(n, dp) == 0 ? false : true;
     }
 }
