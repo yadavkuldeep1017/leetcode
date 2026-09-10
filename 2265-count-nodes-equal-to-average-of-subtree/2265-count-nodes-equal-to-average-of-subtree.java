31d22763eba4360c3e531d924184bedf5d9ab5ab
Kuldeep Yadav <96827805+yadavkuldeep1017@users.noreply.github.com>
2026-09-10 15:59:32 +0530

    Time: 1 ms (56.32%), Space: 44.8 MB (95.34%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.java
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.java
@@ -14,13 +14,14 @@
  * }
  */
 class Solution {
+    int ans = 0;
     public Integer[] average(TreeNode root){
         if(root.left == null && root.right == null){
-            return new Integer[] { 1, root.val, 1};
+            ans++;
+            return new Integer[] { 1, root.val};
         }
         int sum = root.val;
         int count = 1;
-        int ans = 0;
         Integer[] left = null;
         Integer[] right = null;
         if(root.left != null){
@@ -30,28 +31,25 @@ class Solution {
             right = average(root.right);
         }
         if(left != null && right != null){
-            count += left[0] + right[0];
             sum += left[1] + right[1];
-            ans += left[2] + right[2];
+            count += left[0] + right[0];
         }
         else if(left == null){
-            count += right[0];
             sum += right[1];
-            ans += right[2];
-
+            count += right[0];
         } 
         else{
-            count += left[0];
             sum += left[1];
-            ans += left[2];
+            count += left[0] ;
         }
         if(sum / count == root.val){
             ans++;
         }
-        return new Integer[]{count, sum, ans};
+        return new Integer[]{count, sum};
     }
     public int averageOfSubtree(TreeNode root) {
-        Integer[] ans = average(root);
-        return ans[2];
+        average(root);
+        return ans;
+
     }
 }
